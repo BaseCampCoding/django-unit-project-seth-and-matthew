e258a198833af47adaf4dce029fd05eafeb92618
Matthew Wilson <mwilson21@basecampcodingacademy.org>
2021-03-08 16:11:28 -0600

    trying to get the form to work right. currently, inputs do not display

--- questions/forms.py
+++ questions/forms.py
@@ -1,5 +1,7 @@
 from django import forms
-CHOICES=[('1',' 1'),
-         ('2',' 2')]
+CHOICES=[('1','1'),
+         ('2','2'),
+         ('3','3'),
+         ('4','4'),]
 class Answer(forms.ModelForm):
     answer = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect)
