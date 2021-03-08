c41bc018e13c720cbfaa4c5d1e86b5690de1ebfb
Matthew Wilson <mwilson21@basecampcodingacademy.org>
2021-03-08 11:30:45 -0600

    registered users to admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CreateUserForm, ChangeUserForm
+from .models import CustomUser
 
 # Register your models here.
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CreateUserForm
+    form = ChangeUserForm
+    model = CustomUser
+
+
+UserAdmin.fieldsets += (
+    (
+        "Custom fields set",
+        {"fields": ("points", "streak", "longest_streak")},
+    ),
+)
+
+admin.site.register(CustomUser, CustomUserAdmin)
