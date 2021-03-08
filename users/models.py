f4735cc3e2e8406b23bb409913c2738a0883fe10
Matthew Wilson <mwilson21@basecampcodingacademy.org>
2021-03-08 10:58:18 -0600

    added custom user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
-# Create your models here.
+class Customuser(AbstractUser):
+    points = models.PositiveIntegerField()
+    streak = models.PositiveIntegerField()
+    longest_streak = models.PositiveIntegerField()
+    answered = models.JSONField()
+    #badges = ArrayField(models.PositiveIntegerField(), size=)
