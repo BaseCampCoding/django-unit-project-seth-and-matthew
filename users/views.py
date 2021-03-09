ac3510fe9ab7c669ca5a5128d7b93f55667208a3
Matthew Wilson <mwilson21@basecampcodingacademy.org>
2021-03-09 10:28:25 -0600

    added it so the central play button carries users to a random question

--- users/views.py
+++ users/views.py
@@ -3,7 +3,8 @@ from .models import CustomUser
 from django.views.generic import CreateView, ListView
 from .forms import CreateUserForm, ChangeUserForm
 from django.urls import reverse_lazy
-
+from questions.models import Question
+import random
 # Create your views here.
 
 
@@ -16,3 +17,8 @@ class SignUpView(CreateView):
 class HomePageView(ListView):
     template_name = "home.html"
     model = CustomUser
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        q_size = Question.objects.count()
+        context['random_number'] = random.randint(1,q_size)
+        return context
