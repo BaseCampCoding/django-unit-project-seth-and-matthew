4fb500b1bd2659c80463ebadda731c46b83f40d3
Matthew Wilson <mwilson21@basecampcodingacademy.org>
2021-03-09 15:25:36 -0600

    working on specific category choosing

--- users/views.py
+++ users/views.py
@@ -8,8 +8,23 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
 import random
 
+all_categories = ["Python"] # IMPORTANT
+
 # Create your views here.
 
+def get_possible_questions_id(q_category):
+    """
+    used to generate a question for a given category.
+    this function returns the PK of a random question object that fits the question category.
+    """
+    q_ids = Question.objects.filter(category=q_category).values_list("pk", flat=True)
+    print(q_category)
+    q_ids = list(q_ids)
+    maxi_id = max(q_ids)
+    ran = None
+    while not ran in q_ids:
+        ran = random.randint(1, maxi_id)
+    return ran
 
 class SignUpView(CreateView):
     form_class = CreateUserForm
@@ -25,6 +40,8 @@ class HomePageView(ListView):
         context = super().get_context_data(**kwargs)
         q_size = Question.objects.count()
         context["random_number"] = random.randint(1, q_size)
+        for cat in all_categories:
+            context["cat_" + cat] = get_possible_questions_id(cat)
         return context
 
 
