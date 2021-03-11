0caa1e36ff608aa698322f33d00cac7ad81253a3
Matthew Wilson <mwilson21@basecampcodingacademy.org>
2021-03-11 09:47:28 -0600

    added badges for Python and HTML, fixed bug where signed out users couldn't see home page

--- users/models.py
+++ users/models.py
@@ -9,5 +9,6 @@ class CustomUser(AbstractUser):
     streak = models.PositiveIntegerField(default=0)
     longest_streak = models.PositiveIntegerField(default=0)
     badges = models.CharField(max_length=100, default="")
+    completed_problems = models.CharField(max_length=200, default="") # e.g. 4Python10Math2Science
     challenge_streak = models.PositiveIntegerField(default=0)
     max_challenge_streak = models.PositiveIntegerField(default=0)
