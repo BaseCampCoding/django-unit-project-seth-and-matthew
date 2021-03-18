d74bd15ef584fb605e0f9ae2ea0f8996a23fcb1b
Seth Hogue <shogue21@basecampcodingacademy.org>
2021-03-18 15:00:18 -0500

    increased maxlength of question title

--- questions/models.py
+++ questions/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.urls import reverse
+
 # from django.contrib.postgres.fields import ArrayField
 class Question(models.Model):
-    title = models.CharField(max_length=50, default="A very good title")
+    title = models.CharField(max_length=250, default="A very good title")
     category = models.CharField(max_length=30)
     correct_answer = models.PositiveIntegerField(default=1)
     description = models.TextField()
-    possible_answers = [1,2,3,4]
+    possible_answers = [1, 2, 3, 4]
     # def get_absolute_url(self):
     #     return reverse("home")
-    def __str__ (self):
+    def __str__(self):
         return f"{str(self.id)} -> {self.title}"
